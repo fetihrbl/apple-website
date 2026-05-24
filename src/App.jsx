dc4cae0e43698e3d28c00ac645316d794b8c37a6
Fatih Erbil <fatiherbil@Fatihs-MacBook-Air.local>
2026-05-24 14:45:32 -0700

    Implement Features section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Higlights from './components/Higlights'
 import Model from './components/Model'
+import Features from './components/Features'
+
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
       <Hero />
       <Higlights />
       <Model />
+      <Features />
     </main>
   )
 }
